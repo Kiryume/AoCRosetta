8934fdb390ed67e08aaceb08c6e59c8de7123b2a
Kirsten Pleskot <kirstenpleskot@gmail.com>
2025-12-07 22:02:39 +0100

    funnify

--- 2025/day_6/second_star/src/main.rs
+++ 2025/day_6/second_star/src/main.rs
@@ -6,40 +6,48 @@ fn main() {
         .last()
         .map(|l| l.split_whitespace().collect::<Vec<_>>())
         .unwrap();
-    let res = transpose(
-        &input_file
-            .lines()
-            .take(input_file.lines().count() - 1)
-            .map(|l| l.split("").collect::<Vec<_>>())
-            .collect::<Vec<_>>(),
-    )
-    .iter()
-    .map(|l| l.iter().map(|s| s.to_string()).collect::<String>())
-    .map(|l| l.trim().to_string())
-    .reduce(|a, b| format!("{}\n{}", a, b))
-    .unwrap()
-    .split("\n\n")
-    .map(|hw| {
-        hw.split_whitespace()
-            .map(|n| n.trim().parse::<u64>().unwrap())
-            .collect::<Vec<_>>()
-    })
-    .enumerate()
-    .map(|(i, hw)| match operators[i] {
-        "+" => hw.iter().sum::<u64>(),
-        "*" => hw.iter().product::<u64>(),
-        _ => panic!("Unknown operator"),
-    })
-    .sum::<u64>();
+    let res = input_file
+        .lines()
+        .take(input_file.lines().count() - 1)
+        .map(|l| l.split("").collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+        .transpose()
+        .iter()
+        .map(|l| l.iter().map(|s| s.to_string()).collect::<String>())
+        .map(|l| l.trim().to_string())
+        .reduce(|a, b| format!("{}\n{}", a, b))
+        .unwrap()
+        .split("\n\n")
+        .map(|hw| hw.split_whitespace().map(|n| n.parse::<u64>().unwrap()))
+        .enumerate()
+        .map(|(i, hw)| match operators[i] {
+            "+" => hw.sum::<u64>(),
+            "*" => hw.product::<u64>(),
+            _ => panic!("Unknown operator"),
+        })
+        .sum::<u64>();
     println!("{}", res);
 }
 
-fn transpose<'a>(matrix: &'a Vec<Vec<&'a str>>) -> Vec<Vec<&'a str>> {
-    let mut transposed = vec![vec![""; matrix.len()]; matrix[0].len()];
-    for i in 0..matrix.len() {
-        (0..matrix[0].len()).for_each(|j| {
-            transposed[j][i] = matrix[i][j];
-        });
+trait Transpose<T> {
+    fn transpose(self) -> Vec<Vec<T>>;
+}
+
+impl<T> Transpose<T> for Vec<Vec<T>> {
+    fn transpose(self) -> Vec<Vec<T>> {
+        if self.is_empty() {
+            return vec![];
+        }
+        let row_len = self[0].len();
+        let mut transposed = Vec::with_capacity(row_len);
+        for _ in 0..row_len {
+            transposed.push(Vec::with_capacity(self.len()));
+        }
+        for row in self {
+            for (j, val) in row.into_iter().enumerate() {
+                transposed[j].push(val);
+            }
+        }
+        transposed
     }
-    transposed
 }
